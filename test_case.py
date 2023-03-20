24cb6277ecb2711da14378704aac7241c84e98c6
Fahad Nazeer <Fahad5151>
2023-03-20 15:02:33 +0500

    pipeline test

--- test_case.py
+++ test_case.py
@@ -3,7 +3,7 @@ from app import app
 url = "http://127.0.0.1:5000"
 
 def test_index_route():
-    response = app.test_client.get(url)
+    response = app.test_client().get(url)
     assert response.status_code == 200
     assert "Hello World!" in response.text
 
